2fa2b617ffbc2410e7b11dafbe599f69baeec87f
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-10-14 23:53:08 +0800

    LC110: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -522,4 +522,15 @@ public class Solution {
 		}
         return resultR;
     }
+	
+	/**
+	 *  [Easy]
+	 *  #110. Balanced Binary Tree
+	 *  Given a binary tree, determine if it is height-balanced.
+	 *  For this problem, a height-balanced binary tree is defined as a binary tree 
+	 *  in which the depth of the two subtrees of every node never differ by more than 1.
+	 */
+	public boolean isBalanced(TreeNode root) {
+        return false;
+    }
 }
