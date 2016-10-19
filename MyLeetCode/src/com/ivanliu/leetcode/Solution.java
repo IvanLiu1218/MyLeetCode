c9cd78a4616af2168b377218236a0bce0c9b3917
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-10-20 00:31:06 +0800

    LC189: update description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -878,6 +878,10 @@ public class Solution {
 	 *  
 	 *  Note:
 	 *  Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+	 *  
+	 *  Hint:
+	 *  Could you do it in-place with O(1) extra space?
+	 *  Related problem: Reverse Words in a String II
 	 */
 	public void rotate(int[] nums, int k) {
         
