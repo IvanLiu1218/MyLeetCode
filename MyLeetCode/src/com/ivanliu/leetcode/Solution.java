c972fd69cc9e569e7b9b236714aaf8342b2dec1f
Ivan Liu <ivan.liu@blackboard.com>
2016-10-21 17:42:33 +0800

    LC204: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1023,4 +1023,21 @@ public class Solution {
     	}
         return node;
     }
+    
+    /**
+     *  [Easy]
+     *  #204. Count Primes
+     *  
+     *  Description:
+     *  Count the number of prime numbers less than a non-negative number, n.
+     *  
+     *  Hint:
+     *  Let's start with a isPrime function. To determine if a number is prime, 
+     *  we need to check if it is not divisible by any number less than n. 
+     *  The runtime complexity of isPrime function would be O(n) and hence counting the total prime numbers 
+     *  up to n would be O(n2). Could we do better?
+     */
+    public int countPrimes(int n) {
+        return 0;
+    }
 }
