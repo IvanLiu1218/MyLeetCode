50c399882616cfd741b698f3b0665a97a874bcc0
Ivan Liu <ivan.liu@blackboard.com>
2016-10-21 17:10:28 +0800

    LC203: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -993,4 +993,18 @@ public class Solution {
     	}
         return true;
     }
+    
+    /**
+     *  [Easy]
+     *  #203. Remove Linked List Elements
+     *  
+     *  Remove all elements from a linked list of integers that have value val.
+     *  
+     *  Example
+     *  Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+     *  Return: 1 --> 2 --> 3 --> 4 --> 5
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        return null;
+    }
 }
