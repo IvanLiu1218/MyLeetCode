643e2f41e54db358cf51f394f4b13fafa8055f0a
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-10-23 23:56:57 +0800

    LC226: accepted

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1371,4 +1371,26 @@ public class Solution {
             return deque.isEmpty();
         }
     }
+    
+    /**
+     *  [Easy]
+     *  #226. Invert Binary Tree
+     *  Invert a binary tree.
+     *  
+     *       4
+     *     /   \
+     *    2     7
+     *   / \   / \
+     *  1   3 6   9
+     *  
+     *  to
+     *       4
+     *     /   \
+     *    7     2
+     *   / \   / \
+     *  9   6 3   1
+     */
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
 }
