0325d329d18d287000b270a88f5c3cf8a9fac40d
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-10-23 20:47:35 +0800

    LC223: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1294,4 +1294,17 @@ public class Solution {
     	}
         return false;
     }
+    
+    /**
+     *  [Easy]
+     *  #223. Rectangle Area
+     *  
+     *  Find the total area covered by two rectilinear rectangles in a 2D plane.
+     *  Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+     *  
+     *  Assume that the total area is never beyond the maximum possible value of int.
+     */
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        return 0;
+    }
 }
