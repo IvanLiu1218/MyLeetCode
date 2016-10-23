fd1930c89ab8cd72059c9ed019ec020ddb843479
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-10-23 18:16:38 +0800

    LC217: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1246,4 +1246,16 @@ public class Solution {
     	}
     	return head;
     }
+    
+    /**
+     *  [Easy]
+     *  #217. Contains Duplicate
+     *  
+     *  Given an array of integers, find if the array contains any duplicates. 
+     *  Your function should return true if any value appears at least twice in the array, 
+     *  and it should return false if every element is distinct.
+     */
+    public boolean containsDuplicate(int[] nums) {
+        return false;
+    }
 }
