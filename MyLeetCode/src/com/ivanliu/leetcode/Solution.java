6c70d9e28a9302afe5b9347255de0a5e0151fe0e
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-06 23:48:21 +0800

    LC242: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1601,4 +1601,21 @@ public class Solution {
         node.val = node.next.val;
         node.next = node.next.next;
     }
+    
+    /**
+     *  [Easy]
+     *  #242. Valid Anagram
+     *  
+     *  Given two strings s and t, write a function to determine if t is an anagram of s.
+     *  
+     *  For example,
+     *  s = "anagram", t = "nagaram", return true.
+     *  s = "rat", t = "car", return false.
+     *  
+     *  Note:
+     *  You may assume the string contains only lowercase alphabets.
+     */
+    public boolean isAnagram(String s, String t) {
+        return false;
+    }
 }
