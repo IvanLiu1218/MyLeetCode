acae12ee9a3894c2ba2cb17eeb151d0fef3143c5
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-07 22:38:33 +0800

    LC258: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1676,4 +1676,20 @@ public class Solution {
     		if (node.right != null) binaryTreePaths(result, path + "->" + node.right.val, node.right);
     	}
     }
+    
+    /**
+     *  [Easy]
+     *  #258. Add Digits
+     *  
+     *  Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+     *  
+     *  For example:
+     *  Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+     *  
+     *  Follow up:
+     *  Could you do it without any loop/recursion in O(1) runtime?
+     */
+    public int addDigits(int num) {
+        return 0;
+    }
 }
