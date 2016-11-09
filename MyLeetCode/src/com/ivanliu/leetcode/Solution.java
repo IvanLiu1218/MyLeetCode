d86cb1276a78d33cb23bf44fcfec1097e2c16ac3
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-09 23:17:50 +0800

    LC290: accepted

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1772,4 +1772,24 @@ public class Solution {
  	        nums[j] = temp;
         }
     }
+    
+    /**
+     *  [Easy]
+     *  #290. Word Pattern
+     *  
+     *  Given a pattern and a string str, find if str follows the same pattern.
+     *  Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+     *  
+     *  Examples:
+     *  pattern = "abba", str = "dog cat cat dog" should return true.
+     *  pattern = "abba", str = "dog cat cat fish" should return false.
+     *  pattern = "aaaa", str = "dog cat cat dog" should return false.
+     *  pattern = "abba", str = "dog dog dog dog" should return false.
+     *  
+     *  Notes:
+     *  You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+     */
+    public boolean wordPattern(String pattern, String str) {
+        return false;
+    }
 }
