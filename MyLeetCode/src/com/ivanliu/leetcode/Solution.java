812bd6048fae1e721f9ca5d50af5e842e141e179
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-17 00:29:31 +0800

    LC349: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1988,4 +1988,21 @@ public class Solution {
     	}
     	return new String(cs);
     }
+    
+    /**
+     *  [Easy]
+     *  #349. Intersection of Two Arrays
+     *  
+     *  Given two arrays, write a function to compute their intersection.
+     *  
+     *  Example:
+     *  Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+     *  
+     *  Note:
+     *  Each element in the result must be unique.
+     *  The result can be in any order.
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        return null;
+    }
 }
