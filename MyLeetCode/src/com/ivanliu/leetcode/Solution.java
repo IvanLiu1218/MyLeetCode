9382fe7c3a919dea7363043cdc618a961057a348
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-17 00:13:01 +0800

    LC345: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1946,4 +1946,23 @@ public class Solution {
         }
         return sb.toString();
     }
+    
+    /**
+     *  [Easy]
+     *  #345. Reverse Vowels of a String
+     *  
+     *  Write a function that takes a string as input and reverse only the vowels of a string.
+     *  
+     *  Example 1:
+     *  Given s = "hello", return "holle".
+     *  
+     *  Example 2:
+     *  Given s = "leetcode", return "leotcede".
+     *  
+     *  Note:
+     *  The vowels does not include the letter "y".
+     */
+    public String reverseVowels(String s) {
+        return null;
+    }
 }
