7e914fe8a5210e549dbac4bf2c53354b3c2d21e0
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-20 22:43:47 +0800

    LC404: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -2415,4 +2415,23 @@ public class Solution {
     	}
     }
     
+    /**
+     *  [Easy]
+     *  #404. Sum of Left Leaves
+     *  
+     *  Find the sum of all left leaves in a given binary tree.
+     *  
+     *  Example:
+     *  
+     *      3
+     *     / \
+     *    9  20
+     *      /  \
+     *     15   7
+     *  
+     *  There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+    	return 0;
+    }
 }
