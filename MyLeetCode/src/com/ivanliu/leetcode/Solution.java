d1876be44c9f79c1fce566ba546690d58a7c8426
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-23 00:08:25 +0800

    LC437: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -2671,7 +2671,7 @@ public class Solution {
     
     /**
      *  [Easy]
-     *  415. Add Strings
+     *  #415. Add Strings
      *  Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
      *  
      *  Note:
@@ -2723,5 +2723,34 @@ public class Solution {
     		sb.insert(0, (char)(prev + 48));
     	}
         return sb.toString();
+    }
+    
+    /**
+     *  [Easy]
+     *  #437. Path Sum III
+     *  You are given a binary tree in which each node contains an integer value.
+     *  Find the number of paths that sum to a given value.
+     *  The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+     *  The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+     *  
+     *  Example:
+     *  root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+     *  
+     *        10
+     *       /  \
+     *      5   -3
+     *     / \    \
+     *    3   2   11
+     *   / \   \
+     *  3  -2   1
+     *  
+     *  Return 3. The paths that sum to 8 are:
+     *  
+     *  1.  5 -> 3
+     *  2.  5 -> 2 -> 1
+     *  3. -3 -> 11
+     */
+    public int pathSum(TreeNode root, int sum) {
+        return 0;
     }
 }
