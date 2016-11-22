69bef144ecf263fe0a350a3bb81a35cf787a1193
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-11-22 23:49:49 +0800

    LC415: add description

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -2668,4 +2668,19 @@ public class Solution {
     	}
     	return max;
     }
+    
+    /**
+     *  [Easy]
+     *  415. Add Strings
+     *  Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+     *  
+     *  Note:
+     *  1. The length of both num1 and num2 is < 5100.
+     *  2. Both num1 and num2 contains only digits 0-9.
+     *  3. Both num1 and num2 does not contain any leading zero.
+     *  4. You must not use any built-in BigInteger library or convert the inputs to integer directly.
+     */
+    public String addStrings(String num1, String num2) {
+        return null;
+    }
 }
