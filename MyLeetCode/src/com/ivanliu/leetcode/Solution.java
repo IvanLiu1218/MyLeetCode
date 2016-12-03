b54593daa3f80aa73c8052c1370f6a04ca86a023
IvanLiu1218 <ivanliu1218@hotmail.com>
2016-12-03 23:43:50 +0800

    LC058: add #058 and accepted

--- MyLeetCode/src/com/ivanliu/leetcode/Solution.java
+++ MyLeetCode/src/com/ivanliu/leetcode/Solution.java
@@ -1116,6 +1116,33 @@ public class Solution {
 	
 	/**
 	 *  [Easy]
+	 *  #058. Length of Last Word
+	 *  
+	 *  Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+	 *  
+	 *  If the last word does not exist, return 0.
+	 *  
+	 *  Note: A word is defined as a character sequence consists of non-space characters only.
+	 *  
+	 *  For example, 
+	 *  Given s = "Hello World",
+	 *  return 5.
+	 */
+	public int lengthOfLastWord(String s) {
+		if (s == null) return 0;
+		int length = 0;
+		for (int i = s.length() - 1; i >= 0; --i) {
+			if (s.charAt(i) == ' ') {
+				if (length == 0) continue;
+				else return length;
+			}
+			else length++;
+		}
+		return length;
+    }
+	
+	/**
+	 *  [Easy]
 	 *  #067. Add Binary
 	 *  
 	 *  Given two binary strings, return their sum (also a binary string).
